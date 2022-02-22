1d4023cef0f46b47f99ffd2fbe1d7bb3f4f40781
Peter_Fisker <pet5631@gmail.com>
2022-02-22 16:07:07 +0100

    Separated the plotting part from the training part in train_agent.
    Plotting part is now in its own script.

--- train_agent.py
+++ train_agent.py
@@ -7,7 +7,7 @@
 import json
 import sys
 from time import time
-import dill as pickle
+
 
 import numpy as np
 from tqdm import tqdm
@@ -257,63 +257,3 @@ if __name__ == "__main__":
     
     helpers.dump_pickle(data,'','_results.pickle')
 
-    # %% Load pickle
-    
-    data = helpers.load_pickle('','_results.pickle')
-    Agent = data['Agent']
-    R_log = data['R_log']
-    R_max_log = data['R_max']
-    R_min_log = data['R_min']
-    R_mean_log = data['R_mean']
-    setting = data['settings']
-
-    # %% PLOT
-    print("Starts plotting")
-
-    # Get the Logs in power decibel
-    R_log_db = 10 * np.log10(R_log)
-    R_max_log_db = 10 * np.log10(R_max_log)
-    R_min_log_db = 10 * np.log10(R_min_log)
-    R_mean_log_db = 10 * np.log10(R_mean_log)
-    #Misalignment_log_dB = R_max_log_db - R_log_db
-    Misalignment_log_dB = R_log_db - R_max_log_db
-    Meanalignment_log_dB = R_mean_log_db - R_max_log_db
-    Minalignment_log_dB = R_min_log_db - R_max_log_db
-
-    # plots.mean_reward(R_max_log, R_mean_log, R_min_log, R_log,
-    #                   ["R_max", "R_mean", "R_min", "R"], "Mean Rewards")
-
-    plots.ECDF(np.mean(Misalignment_log_dB[-3:-1,:], axis=0))
-    plots.Relative_reward(np.mean(Misalignment_log_dB[-3:-1,:], axis=0), np.mean(Meanalignment_log_dB[-3:-1,:], axis=0), np.mean(Minalignment_log_dB[-3:-1,:], axis=0))
-
-    plots.mean_reward(R_max_log_db[-3:-1,:], R_mean_log_db[-3:-1,:], R_min_log_db[-3:-1,:], R_log_db[-3:-1,:],
-                      ["R_max", "R_mean", "R_min", "R"], "Mean Rewards db",
-                      db=True)
-
-    # plots.positions(pos_log, r_lim)
-
-    # X-db misalignment probability
-    x_db = 3
-    ACC_xdb = helpers.misalignment_prob(np.mean(R_log_db, axis=0),
-                                        np.mean(R_max_log_db, axis=0), x_db)
-    print(F"{x_db}-db Mis-alignment probability: {ACC_xdb:0.3F} for full length")
-
-    NN = 1000
-    ACC_xdb_NL = helpers.misalignment_prob(np.mean(R_log_db[:, -NN:], axis=0),
-                                           np.mean(R_max_log_db[:, -NN:], axis=0), x_db)
-    print(F"{x_db}-db Mis-alignment probability: {ACC_xdb_NL:0.3F} for the last {NN}")
-
-    ACC_xdb_NF = helpers.misalignment_prob(np.mean(R_log_db[:, 0:NN], axis=0),
-                                           np.mean(R_max_log_db[:, 0:NN], axis=0), x_db)
-    print(F"{x_db}-db Mis-alignment probability: {ACC_xdb_NF:0.3F} for the first {NN}")
->>>>>>> Hierarchical-Codebooks:main.py
-
-    print("Done")
-
-
-# var_accums = np.zeros(40)
-# for j in range(40):
-#     for i in range(int(np.floor(len(R_log_db[j])/30))):
-#         var_accums[j] += np.var(R_log_db[j,i*30:i*30 + 30]) - np.mean(R_log_db[j,i*30:i*30 + 30])
-#
-# var_accums /= 1300
