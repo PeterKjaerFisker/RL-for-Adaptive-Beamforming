2fce053e5f13fa4a8c0add2ccd3826958c6c1017
Peter_Fisker <pet5631@gmail.com>
2022-03-01 15:55:31 +0100

    Small formatting and clean-up

--- data_analysis.py
+++ data_analysis.py
@@ -10,11 +10,11 @@ import sys
 import helpers
 import plots
 
-
 cmd_input = sys.argv
 if len(cmd_input) > 1:
     SETTING = sys.argv[1]
 else:
+    # SETTING = "pedestrian_LOS_8_users_20000_steps"
     SETTING = "car_urban_LOS_16_users_10000_steps"
 
 # Load Settings for simulation
@@ -43,16 +43,21 @@ Misalignment_log_dB = R_log_db - R_max_log_db
 Meanalignment_log_dB = R_mean_log_db - R_max_log_db
 Minalignment_log_dB = R_min_log_db - R_max_log_db
 
-plots.ECDF(Save, np.mean(Misalignment_log_dB[-3:-1, :], axis=0))
-plots.Relative_reward(Save, np.mean(Misalignment_log_dB, axis=0), np.mean(Meanalignment_log_dB, axis=0),
+plots.ECDF(Save, np.mean(Misalignment_log_dB[-100:, :], axis=0))
+
+plots.Relative_reward(Save,
+                      np.mean(Misalignment_log_dB, axis=0),
+                      np.mean(Meanalignment_log_dB, axis=0),
                       np.mean(Minalignment_log_dB, axis=0))
+
+plots.Relative_reward(Save,
+                      np.mean(Misalignment_log_dB[-1000:-800, :], axis=0),
+                      np.mean(Meanalignment_log_dB[-1000:-800, :], axis=0),
+                      np.mean(Minalignment_log_dB[-1000:-800, :], axis=0))
+
 plots.stability(Save, R_log_db, 50)
 
 plots.mean_reward(Save, R_max_log_db, R_mean_log_db, R_min_log_db, R_log_db,
-                  ["R_max", "R_mean", "R_min", "R"], "Mean Rewards db",
-                  db=True)
-
-plots.mean_reward(Save, R_max_log_db[-4:-3, :], R_mean_log_db[-4:-3, :], R_min_log_db[-4:-3, :], R_log_db[-4:-3, :],
                   ["R_max", "R_mean", "R_min", "R"], "Mean Rewards db",
                   db=True)
 
@@ -60,17 +65,14 @@ plots.mean_reward(Save, R_max_log_db[-4:-3, :], R_mean_log_db[-4:-3, :], R_min_l
 
 # X-db misalignment probability
 x_db = 3
-ACC_xdb = helpers.misalignment_prob(np.mean(R_log_db, axis=0),
-                                    np.mean(R_max_log_db, axis=0), x_db)
+ACC_xdb = helpers.misalignment_prob(np.mean(R_log_db, axis=0), np.mean(R_max_log_db, axis=0), x_db)
 print(F"{x_db}-db Mis-alignment probability: {ACC_xdb:0.3F} for full length")
 
 NN = 1000
-ACC_xdb_NL = helpers.misalignment_prob(np.mean(R_log_db[:, -NN:], axis=0),
-                                       np.mean(R_max_log_db[:, -NN:], axis=0), x_db)
+ACC_xdb_NL = helpers.misalignment_prob(np.mean(R_log_db[:, -NN:], axis=0), np.mean(R_max_log_db[:, -NN:], axis=0), x_db)
 print(F"{x_db}-db Mis-alignment probability: {ACC_xdb_NL:0.3F} for the last {NN}")
 
-ACC_xdb_NF = helpers.misalignment_prob(np.mean(R_log_db[:, 0:NN], axis=0),
-                                       np.mean(R_max_log_db[:, 0:NN], axis=0), x_db)
+ACC_xdb_NF = helpers.misalignment_prob(np.mean(R_log_db[:, 0:NN], axis=0), np.mean(R_max_log_db[:, 0:NN], axis=0), x_db)
 print(F"{x_db}-db Mis-alignment probability: {ACC_xdb_NF:0.3F} for the first {NN}")
 
 print("Done")
